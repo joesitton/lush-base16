534ac2ef5ddad6a13cc70cde35b7e32e82ea1fbc
Joseph Sitton <sittonjoe@gmail.com>
2022-06-24 17:48:38 -0400

    update colors

--- lua/lush_theme/lush-base16.lua
+++ lua/lush_theme/lush-base16.lua
@@ -9,7 +9,7 @@ local theme = require("lush")(function()
         Normal                          { bg = "none" }, -- Normal text
         NormalNC                        { bg = "none" }, -- normal text in non-current windows
         NormalFloat                     { bg = colors.black }, -- Normal text in floating windows.
-        FloatBorder                     { fg = NormalFloat.bg.lighten(10), bg = NormalFloat.bg },
+        FloatBorder                     { fg = NormalFloat.bg.lighten(30), bg = colors.black },
         Comment                         { fg = colors.black.lighten(25), gui = "italic" }, -- Any comment
         -- Conceal                      { }, -- Placeholder characters substituted for concealed text (see 'conceallevel')
         -- Cursor                       { }, -- Character under the cursor
@@ -37,7 +37,7 @@ local theme = require("lush")(function()
         IncSearch                       { fg = colors.black, bg = colors.yellow, gui = "bold"}, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
         Search                          { CursorLine }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
         Substitute                      { bg = colors.orange, fg = colors.black }, -- |:substitute| replacement text highlighting
-        LineNr                          { fg = colors.black.lighten(20), bg = colors.black.lighten(5) }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+        LineNr                          { fg = colors.black.lighten(20), bg = colors.black }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
         SignColumn                      { LineNr }, -- Column where |signs| are displayed
         CursorLineNr                    { fg = colors.white, bg = LineNr.bg, gui = "bold" }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
         MatchParen                      { CursorLine }, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
@@ -46,10 +46,10 @@ local theme = require("lush")(function()
         -- MsgSeparator                 { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
         MoreMsg                         { fg = colors.green, gui = "bold,italic" }, -- |more-prompt|
         NonText                         { Comment, gui = "none" }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
-        Pmenu                           { fg = "none", bg = colors.black.lighten(10) }, -- Popup menu: Normal item.
+        Pmenu                           { fg = "none", bg = colors.black.lighten(5) }, -- Popup menu: Normal item.
         PmenuSel                        { bg = Pmenu.bg.lighten(20) }, -- Popup menu: Selected item.
-        PmenuSbar                       { bg = Pmenu.bg.lighten(40) }, -- Popup menu: Scrollbar.
-        PmenuThumb                      { bg = Pmenu.bg.lighten(60) }, -- Popup menu: Thumb of the scrollbar.
+        PmenuSbar                       { bg = Pmenu.bg.lighten(10) }, -- Popup menu: Scrollbar.
+        PmenuThumb                      { bg = Pmenu.bg.lighten(90) }, -- Popup menu: Thumb of the scrollbar.
         DocMenu                         { bg = Pmenu.bg },
         Question                        { fg = colors.green, gui = "bold,italic" }, -- |hit-enter| prompt and yes/no questions
         -- QuickFixLine                 { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
@@ -321,7 +321,7 @@ local theme = require("lush")(function()
         rainbowcol6                     { fg = colors.blue.saturate(25) },
         rainbowcol7                     { fg = colors.purple.saturate(25) },
 
-        ScrollbarHandle                 { bg = colors.black.lighten(15) },
+        ScrollbarHandle                 { bg = colors.black.lighten(25) },
         ScrollbarSearchHandle           { ScrollbarHandle, fg = IncSearch.bg }, -- Inside the scrollbar
         ScrollbarSearch                 { fg = IncSearch.bg }, -- Outside the scrollbr
         ScrollbarErrorHandle            { ScrollbarHandle, fg = DiagnosticError.fg },
